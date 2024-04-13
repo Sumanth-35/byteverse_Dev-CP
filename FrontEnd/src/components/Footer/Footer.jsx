21b9859ea29487878edcefa7e684ddc2a76558ef
Anurag Bhanu <anurag92017@gmail.com>
2024-04-13 19:17:06 +0530

    Added Animations using AOS

--- FrontEnd/src/components/Footer/Footer.jsx
+++ FrontEnd/src/components/Footer/Footer.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.jpg";
-
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Footer() {
-  
+  useEffect(()=>{
+    Aos.init({duration:2000});
+  },[])
 
   return (
-    <footer className="bg-white" >
+    <footer className="bg-white" data-aos="fade-up">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex ">
           <div className=" mb-6 md:mb-0">
@@ -21,13 +24,20 @@ export default function Footer() {
           </div>
           
           <div className="flex justify-end sm:gap-6 sm:grid-cols-2">
-          <div className="w-2/3 text-xl text-wrap m-2 p-2">Agriverse is a platform that combines the power of machine learning (ML) with agriculture to provide innovative solutions for farmers.It aims to enhance productivity, sustainability, and profitability in agriculture through the application of advanced technology and data analytics.</div>
+          <div className="w-2/3 text-xl text-wrap m-2 p-2">Farm AI is a platform that combines the power of machine learning (ML) with agriculture to provide innovative solutions for farmers.It aims to enhance productivity, sustainability, and profitability in agriculture through the application of advanced technology and data analytics.</div>
             <div>
               <h2 className="mb-6 text-sm font-semibold text-black uppercase ">
                 Resources
               </h2>
               <ul className="text-gray-500 font-medium">
-           
+              {/* <li className="mb-4">
+                  <Link
+                    to="/datacollect"
+                    className="hover:underline hover:text-green-700 "
+                  >
+                    Contribute Data
+                  </Link>
+                </li> */}
                 <li>
                   <Link
                     to="/"
@@ -73,7 +83,7 @@ export default function Footer() {
               © 2023
             </span>
             <span
-              className="hover:underline">Agriverse™. All Rights Reserved.
+              className="hover:underline">Farm AI™. All Rights Reserved.
             </span>
           </Link>
         </div>
