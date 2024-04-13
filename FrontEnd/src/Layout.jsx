89323e6bac4376d0f94553f245bbe8f1693573c0
anuragbhanu <130905151+anuragbhanu@users.noreply.github.com>
2024-04-13 19:21:00 +0530

    Update Layout.jsx

--- FrontEnd/src/Layout.jsx
+++ FrontEnd/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header/Header'
-// import Footer from './components/Footer/Footer'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
@@ -8,7 +8,7 @@ function Layout() {
     <>
     <Header/>
     <Outlet />
-    {/* <Footer /> */}
+    <Footer />
     </>
   )
 }
